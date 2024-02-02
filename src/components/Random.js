6fd02e6a6bc5dd0460b1d8ee16ad02a5ea2e0292
RahulTengli <146551325+RahulTengli@users.noreply.github.com>
2024-02-02 10:02:21 +0530

    Update Random.js

--- src/components/Random.js
+++ src/components/Random.js
@@ -29,7 +29,7 @@ function Random() {
     <div className='bg-green-500 w-full lg:h-[700px] sm:h-[500px] flex flex-col justify-item-stretch items-center rounded-lg gap-10 p-10'>
         <h2 className='uppercase text-2xl underline font-bold p-5 '>A Random Gif</h2>
         {
-          loader ? (<Spinner/>) : (<img className='object-cover lg:h-[350px] sm:h-[200px] w-[350px]' src={gif} width="450px"></img>)
+          loader ? (<Spinner/>) : (<img className='object-cover lg:h-[350px] sm:h-[200px] w-[350px]' alt="img1" src={gif} width="450px"></img>)
         }
 
         <button onClick={clickHandler} className= 'bg-white w-10/12 p-2 font-bold rounded-md text-lg uppercase opacity-80 hover:opacity-100 transition-all duration-200'>Generate</button>
